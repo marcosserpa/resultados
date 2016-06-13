30dcc5c80c00bcb464fc494faaf21fde16a2eef1
Marcos Serpa <marcosserpa@gmail.com>
2016-06-13 15:58:07 -0300

    Removes debugger from JS code Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613175732) do
+ActiveRecord::Schema.define(version: 20160613183520) do
+
+  create_table "pages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.string   "page"
+    t.datetime "date_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pages", ["user_id"], name: "index_pages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
