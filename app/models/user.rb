54f417eb806bae28337a2e449ab26dbed3bcdb67
Marcos Serpa <marcosserpa@gmail.com>
2016-06-13 15:09:54 -0300

    Creates the development DBs Adds the user model basic tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :token, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#save" do
+    context "when tries to save with no email" do
+      it "does not allow to be saved" do
+        user_1 = User.new(name: "Tester", email: nil, token: '12345')
+        user_2 = User.new(name: "Tester", email: '', token: '12345')
+
+        expect(user_1).to be_invalid
+        expect(user_2).to be_invalid
+      end
+    end
+
+    context "when tries to save with an email" do
+      it "allows to be saved" do
+        user = User.new(name: 'Tester', email: 'email', token: '12345')
+
+        expect(user).to be_valid
+      end
+    end
+
+    context "when tries to save with no token" do
+      it "does not allow to be saved" do
+        user_1 = User.new(name: 'Tester', email: 'email', token: nil)
+        user_2 = User.new(name: 'Tester', email: 'email', token: '')
+
+        expect(user_1).to be_invalid
+        expect(user_2).to be_invalid
+      end
+    end
+
+    context "when tries to save with a token" do
+      it "allows to be saved" do
+        user = User.new(name: 'Tester', email: 'email', token: '12345')
+
+        expect(user).to be_valid
+      end
+    end
+  end
 end
